b175cb0daa151af65e9c44e71e22d806d09c5b27
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-08 12:28:25 +0200

    Fix remove book action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,18 @@
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
-
-export default function booksReducer(state = { books: [] }, action = {}) {
-  switch (action.type) {
-    case ADD_BOOK: return state.books.push(action.book);
-    case REMOVE_BOOK: return state.books.pop(action.book);
-    default: return state;
-  }
-}
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 export function addBook(book) {
   return { type: ADD_BOOK, book };
 }
 
-export function removeBook(book) {
-  return { type: REMOVE_BOOK, book };
+export function removeBook(id) {
+  return { type: REMOVE_BOOK, id };
+}
+
+export default function booksReducer(state = { books: [] }, action = {}) {
+  switch (action.type) {
+    case ADD_BOOK: return state.books.push(action.book);
+    case REMOVE_BOOK: return state.books.filter((book) => book.id !== action.id);
+    default: return state;
+  }
 }
