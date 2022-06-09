33d708f4b2887a52bef03e4e919ab074aea544ca
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-09 18:20:00 +0200

    Refactor actions with api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,20 +1,56 @@
+import BookDataService from '../../services/BookService';
+
+const RETRIEVE_BOOKS = 'bookstore/books/RETRIEVE_BOOKS';
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const initBooks = [{ title: 'book1', author: 'author1', id: '1' }, { title: 'book2', author: 'author2', id: '2' }];
 
-export function addBook(book) {
-  return { type: ADD_BOOK, book };
-}
+export const retrieveBooks = () => async (dispatch) => {
+  try {
+    const res = await BookDataService.getAll();
+    dispatch({
+      type: RETRIEVE_BOOKS,
+      books: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export function removeBook(id) {
-  return { type: REMOVE_BOOK, id };
-}
+export const addBook = (book) => async (dispatch) => {
+  try {
+    const res = await BookDataService.create(book);
+    dispatch({
+      type: ADD_BOOK,
+      book: res.data,
+    });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export const removeBook = (id) => async (dispatch) => {
+  try {
+    await BookDataService.remove(id);
+    dispatch({
+      type: REMOVE_BOOK,
+      id,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default function booksReducer(state = initBooks, action = {}) {
   switch (action.type) {
-    case ADD_BOOK: return [...state, action.book];
-    case REMOVE_BOOK: return state.filter((book) => book.id !== action.id);
+    case RETRIEVE_BOOKS:
+      return action.books;
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
     default: return state;
   }
 }
---
+++ src/services/BookService.js
@@ -0,0 +1,19 @@
+import http from '../http-common';
+
+const getAll = () => http.get('/books');
+const get = (id) => http.get(`/books/${id}`);
+const create = (data) => http.post('/books', data);
+const update = (id, data) => http.put(`/books/${id}`, data);
+const remove = (id) => http.delete(`/books/${id}`);
+const removeAll = () => http.delete('/books');
+const findByTitle = (title) => http.get(`/books?title=${title}`);
+const Bookservice = {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+  removeAll,
+  findByTitle,
+};
+export default Bookservice;
