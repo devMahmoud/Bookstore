23b9a6784e68d5723a6e1c94706ee5f35f26ac22
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-10 14:10:33 +0200

    Add styles to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import imgUrl from '../../img/person-48.png';
 
 const Navbar = () => (
   <nav>
-    <div>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
+    <span className="bookstore-title">
+      Bookstore CMS
+    </span>
+    <div className="nav-links">
+      <Link to="/" className="books-link">Books</Link>
+      <Link to="/categories" className="categories-link">Categories</Link>
     </div>
+    <img className="profile-icon" src={imgUrl} alt="" />
   </nav>
 );
 
